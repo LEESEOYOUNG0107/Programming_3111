94841a987923c733971d794a87b2f992ad359fbe
LEESEOYOUNG0107 <s92202763@gmail.com>
2026-03-23 14:12:29 +0900

    counterJs addEventListener()

---
+++ 02_counter/main.js
@@ -0,0 +1,8 @@
+let count = 0;
+const resultH1 = document.getElementsByTagName("h2")[0];
+const plusBtn = document.getElementsByTagName("button")[0];
+
+plusBtn.addEventListener("click", function () {
+    count++;
+    resultH1.innerHTML = count; //증가한 숫자 화면에 표시
+}); 
