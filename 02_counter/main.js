16257e5f784a63fe16044624c602c4bc1616727c
LEESEOYOUNG0107 <s92202763@gmail.com>
2026-03-23 14:23:20 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,13 +1,19 @@
 let count = 0;
-const resultH1 = document.getElementsByTagName("h2")[0];
-const plusBtn = document.getElementsByTagName("button")[0];
+// const resultH1 = document.getElementsByTagName("h2")[0];
+const resultH1 = document.getElementById("result");
+// const plusBtn = document.getElementsByTagName("button")[0];
 
 // plusBtn.addEventListener("click", function () {
 //     count++;
 //     resultH1.innerHTML = count; //증가한 숫자 화면에 표시
 // }); 
 
-plusBtn.onclick = () => {
+// plusBtn.onclick = () => {
+//     count++;
+//     resultH1.innerHTML = count; //증가한 숫자 화면에 표시
+// }; 
+
+function plus(){
     count++;
-    resultH1.innerHTML = count; //증가한 숫자 화면에 표시
-}; 
+    resultH1.innerHTML = count;
+}
