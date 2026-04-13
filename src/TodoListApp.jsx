d8e4f1cbbf6c708a2d29d2785bcccb28ca7a11bd
LEESEOYOUNG0107 <s92202763@gmail.com>
2026-04-13 14:49:58 +0900

    TodoApp form -> TodoAdder, li -> TodoItem, ul -> TodoList

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,26 +1,14 @@
 import "./todolist.css"
-import Button from "./components/Button.jsx"
-import TodoItemempty from './components/TodoItemEmpty.jsx';
 import TodoHeader from './components/TodoHeader.jsx';
-import Checkbox from './components/Checkbox.jsx';
+import TodoAdder from './components/TodoAdder.jsx';
+import TodoList from './components/TodoList.jsx';
 
-export default function TodoListApp(){
-    return(
+export default function TodoListApp() {
+    return (
         <div className="todo">
-            <TodoHeader/>
-            <form className="todo__form">
-                <input type="text" placeholder="할 일을 입력하세요." className="todo__input"/>
-                <Button type="submit" className="todo__button todo__button--add">Add</Button>
-                <ul className="todo__list">
-                    <TodoItemempty/>
-                    <li className="todo__item todo__item--complete">
-                        <Checkbox id="1">할 일1</Checkbox>
-                        <Button className="todo__button todo__button--edit">✏️</Button>
-                        <Button className="todo__button todo__button--delete">❌</Button>
-                    </li>
-
-                </ul>
-            </form>
+            <TodoHeader />
+            <TodoAdder />
+            <TodoList/>
         </div>
     )
 }
