2ab02d66e8f24fcf78bd4017f0cf25074b068983
LEESEOYOUNG0107 <s92202763@gmail.com>
2026-04-20 14:38:12 +0900

    TodoListApp toggleTodo()

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,15 @@
 import TodoItemEmpty from "./TodoItemEmpty"
 import TodoItem from "./TodoItem"
 
-export default function TodoList({todos}) {
+export default function TodoList({todos, toggleTodo}) {
     return (
         <ul className="todo__list">
             {/* todos에 todo없으면, TodoItemEmpty */}
             {todos.lengh === 0 && <TodoItemEmpty />}
 
             {/* todos에 todo있으면, TodoItem */}
-            {todos.length > 0 && 
-                todos.map(
-                    (todo) => < TodoItem key={todo.id} todo={todo}/>
+            {todos.length > 0 && todos.map((todo) =>
+                < TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
             )}
         </ul>
     )
