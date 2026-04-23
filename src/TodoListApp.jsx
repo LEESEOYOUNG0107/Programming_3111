fd2395933d0f64fd423a6acf198edd1d3bc28109
LEESEOYOUNG0107 <s92202763@gmail.com>
2026-04-23 11:11:10 +0900

    TodoListApp LocalStorage, TodoListApp ✌️

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -2,7 +2,7 @@ import "./todolist.css"
 import TodoHeader from './components/TodoHeader.jsx';
 import TodoAdder from './components/TodoAdder.jsx';
 import TodoList from './components/TodoList.jsx';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 class Todo{
     constructor (text){
@@ -11,8 +11,27 @@ class Todo{
         this.isCompleted = false; //완료 여부: 기본 값 false
     }
 }
+const TODOS_STORAGE_KEY = "todos";
 export default function TodoListApp() {
-    const [todos, setTodos] = useState([]); //할 일 목록: 기본값 빈 리스트
+    //localstorage에서 저장된 할일 목록 가져오기
+    const initTodos = () => {
+        const savedTodos = localStorage.getItem(TODOS_STORAGE_KEY);
+        return savedTodos ? JSON.parse(savedTodos) : [];
+
+    }
+    //LocalStorage에서 저장된 할일 목록 가져오자
+    const initTodos = () => {
+        const savedTodos = localStorage.getItem(TODOS_STORAGE_KEY);
+        return savedTodos ? JSON.parse(savedTodos) : [];
+    }
+
+    const [todos, setTodos] = useState(initTodos); //할일 목록: 기본값 빈 리스트
+
+    //todos가 바뀌면, LocalStroage에 저장하자
+    useEffect(() => {
+        localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+    }, [todos]);     //[](mount할 때 한번 실행), [todos]에 있는 state가 바뀌면, 그 앞 함수정의 를 호출하자
+    
     const addTodo = (text) => setTodos((todos) => [
         //이전 todos 복사
         ...todos,
@@ -33,16 +52,25 @@ export default function TodoListApp() {
     }
 
     const deleteTodo = (id) => {
-        //id가 같지 않은 todo만 복사하자(filter())
+        //id가 같지 않은 todo만 복사하자(filter()) 같은 건 복사안 되서 안 나옴
         setTodos(
             (todos) => todos.filter((todo) => todo.id !== id)
+        )
+    }
+
+    const editTodo = (id, newText) => {
+        //todos에서 하나씩 todo를 꺼내고, id가 같은 todo 찾아서, text를 newText로 수정하자
+        setTodos(
+            todos.map((todo) => 
+                todo.id === id ? {...todo, text: newText} : todo
+            )
         )
     }
     return (
         <div className="todo">
             <TodoHeader />
             <TodoAdder addTodo={addTodo}/>
-            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} editTodo={editTodo}/>
         </div>
     )
 }
